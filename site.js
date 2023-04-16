d4c857babd06497bd13dd4185fa57d1e06ee06e1
LHensman <lukehensman7@gmail.com>
2023-04-16 23:13:36 +0100

    structure changes and contact function

--- site.js
+++ site.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route to handle form submissions
@@ -15,19 +18,33 @@ app.post('/submit_form', (req, res) => {
   // Validate and sanitize form data
 
   // Read existing JSON file and parse it into a JavaScript object
-  const formData = { name, email, message };
-  fs.readFile('form_data.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    const existingData = JSON.parse(data);
+  fs.readFile('messages.json', 'utf8', (err, data) => {
+    if (err && err.code !== 'ENOENT') throw err; // Handle file not found error
+    let existingData = [];
+
+    if (data) {
+      try {
+        existingData = JSON.parse(data);
+      } catch (err) {
+        console.error('Error parsing JSON:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+    }
 
     // Append form data to the JavaScript object
+    const formData = { name, email, message };
     existingData.push(formData);
 
     // Write updated data back to the JSON file
     fs.writeFile('messages.json', JSON.stringify(existingData), (err) => {
-      if (err) throw err;
+      if (err) {
+        console.error('Error writing to JSON file:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
       console.log('Form data appended to JSON file successfully.');
-      res.send('Form data submitted and appended to JSON file successfully.');
+      res.redirect('http://localhost:3000/index.html');
     });
   });
 });
