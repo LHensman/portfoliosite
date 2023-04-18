a1b58da9eb55370a522f34e17f5a67b319507114
LHensman <lukehensman7@gmail.com>
2023-04-18 20:59:39 +0100

    different passes

--- site.js
+++ site.js
@@ -1,53 +1,80 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const nodemailer = require('nodemailer');
 const app = express();
 
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Route to handle form submissions
+function getFirstLineFromServer() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch('http://localhost:9090/get_pass'); // Change the URL to match your Express server's URL
+      if (!response.ok) {
+        throw new Error('Failed to fetch first line');
+      }
+      const data = await response.text();
+      const firstLine = data.trim(); // Trim any leading/trailing white spaces
+      resolve(firstLine);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+async function getPassword() {
+  try {
+    const result = await getFirstLineFromServer();
+    console.log('Success:', result);
+    return result;
+  } catch (err) {
+    console.error('Error:', err);
+  }
+};
+
+(async () => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'mailbot7070@gmail.com',
+      pass: await getPassword() // Use await to get the resolved value
+    }
+  });
+})();
+
 app.post('/submit_form', (req, res) => {
-  // Get form data from request body
+  console.log('Form submitted');
   const name = req.body.name;
   const email = req.body.email;
   const message = req.body.message;
 
   // Validate and sanitize form data
 
-  // Read existing JSON file and parse it into a JavaScript object
-  fs.readFile('messages.json', 'utf8', (err, data) => {
-    if (err && err.code !== 'ENOENT') throw err; // Handle file not found error
-    let existingData = [];
+  // Compose email message
+  const mailOptions = {
+    from: 'mailbot7070@gmail.com', // replace with your email address
+    to: 'lukehensman7@gmail.com', // replace with recipient's email address
+    subject: 'Form Submission',
+    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`
+  };
 
-    if (data) {
-      try {
-        existingData = JSON.parse(data);
-      } catch (err) {
-        console.error('Error parsing JSON:', err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
+
+  // Send email using the transporter
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Internal Server Error');
+    } else {
+      console.log('Email sent:', info.response);
+      res.redirect('http://localhost:8080/index.html');
     }
-
-    // Append form data to the JavaScript object
-    const formData = { name, email, message };
-    existingData.push(formData);
-
-    // Write updated data back to the JSON file
-    fs.writeFile('messages.json', JSON.stringify(existingData), (err) => {
-      if (err) {
-        console.error('Error writing to JSON file:', err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-      console.log('Form data appended to JSON file successfully.');
-      res.redirect('http://portfolioapp-env.eba-mrcvtfmi.eu-west-2.elasticbeanstalk.com/index.html');
-    });
   });
 });
+
 
 // Start the server
 app.listen(8080, () => {
