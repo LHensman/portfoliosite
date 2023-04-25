59e589c2a3496d55538eabefedab31b28d32602b
LHensman <lukehensman7@gmail.com>
2023-04-25 21:50:27 +0100

    Changes

--- site.js
+++ site.js
@@ -1,82 +1,52 @@
-const fs = require('fs');
 const express = require('express');
+const { google } = require('googleapis');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 
 app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function getFirstLineFromServer() {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await fetch('http://localhost:9090/get_pass'); // Change the URL to match your Express server's URL
-      if (!response.ok) {
-        throw new Error('Failed to fetch first line');
-      }
-      const data = await response.text();
-      const firstLine = data.trim(); // Trim any leading/trailing white spaces
-      resolve(firstLine);
-    } catch (err) {
-      reject(err);
-    }
-  });
-}
+const privatekey = JSON.parse(fs.readFileSync('public/mail-384320-a17463cf4599.json'));
 
-async function getPassword() {
-  try {
-    const result = await getFirstLineFromServer();
-    console.log('Success:', result);
-    return result;
-  } catch (err) {
-    console.error('Error:', err);
-  }
-};
-
-(async () => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: 'mailbot7070@gmail.com',
-      pass: await getPassword() // Use await to get the resolved value
-    }
-  });
-})();
-
-app.post('/submit_form', (req, res) => {
-  console.log('Form submitted');
-  const name = req.body.name;
-  const email = req.body.email;
-  const message = req.body.message;
-
-  // Validate and sanitize form data
-
-  // Compose email message
-  const mailOptions = {
-    from: 'mailbot7070@gmail.com', // replace with your email address
-    to: 'lukehensman7@gmail.com', // replace with recipient's email address
-    subject: 'Form Submission',
-    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`
-  };
-
-
-  // Send email using the transporter
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error sending email:', error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      console.log('Email sent:', info.response);
-      res.redirect('http://localhost:8080/index.html');
-    }
-  });
+const jwtClient = new google.auth.JWT({
+  email: privatekey.client_email,
+  key: privatekey.private_key,
+  scopes: ['https://www.googleapis.com/auth/gmail.send'],
 });
 
+app.post('/submit_form', async (req, res) => {
+  const { name, email, message } = req.body;
 
-// Start the server
-app.listen(8080, () => {
-  console.log('Server started');
+  try {
+    // Set the credentials
+    const authorized = await jwtClient.authorize();
+
+    if (authorized.error) {
+      throw new Error(authorized.error);
+    }
+
+    // Send the email
+    const gmail = google.gmail({ version: 'v1', auth: jwtClient });
+    const rawMessage = `From: ${name} <${email}>\nTo: lukehensman7@gmail.com\nSubject: Form Submission\n\n${message}`;
+    const encodedMessage = Buffer.from(rawMessage).toString('base64');
+    await gmail.users.messages.send({
+      userId: 'me',
+      requestBody: { raw: encodedMessage },
+    });
+
+    // Send a success response to the client
+    res.status(200).send('Email sent successfully');
+  } catch (err) {
+    console.error(err);
+    // Send an error response to the client
+    res.status(500).send('Error sending email');
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
 });
