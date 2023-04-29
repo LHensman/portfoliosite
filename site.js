291e53a83a10f7e6e4a43b273ad838c3be98545a
LHensman <lukehensman7@gmail.com>
2023-04-29 17:37:33 +0100

    Form changes and pipeline changes

--- site.js
+++ site.js
@@ -1,47 +1,39 @@
 const express = require('express');
-const { google } = require('googleapis');
 const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const privatekey = JSON.parse(fs.readFileSync('public/mail-384320-a17463cf4599.json'));
-
-const jwtClient = new google.auth.JWT({
-  email: privatekey.client_email,
-  key: privatekey.private_key,
-  scopes: ['https://www.googleapis.com/auth/gmail.send'],
-});
+const apiKey = process.env.SENDGRID_API_KEY;
+if (!apiKey) {
+  console.error('No SendGrid API key found in environment variables');
+  process.exit(1);
+}
+sgMail.setApiKey(apiKey);
 
 app.post('/submit_form', async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
-    // Set the credentials
-    const authorized = await jwtClient.authorize();
+    const msg = {
+      to: 'lukehensman7@gmail.com',
+      from: 'mailbot7070@gmail.com',
+      subject: 'Form Submission',
+      text: message,
+      html: `<p>${message}</p>`,
+    };
+    
+    await sgMail.send(msg);
 
-    if (authorized.error) {
-      throw new Error(authorized.error);
-    }
-
-    // Send the email
-    const gmail = google.gmail({ version: 'v1', auth: jwtClient });
-    const rawMessage = `From: ${name} <${email}>\nTo: lukehensman7@gmail.com\nSubject: Form Submission\n\n${message}`;
-    const encodedMessage = Buffer.from(rawMessage).toString('base64');
-    await gmail.users.messages.send({
-      userId: 'me',
-      requestBody: { raw: encodedMessage },
-    });
-
-    // Send a success response to the client
     res.status(200).send('Email sent successfully');
   } catch (err) {
     console.error(err);
-    // Send an error response to the client
     res.status(500).send('Error sending email');
   }
 });
